88922fa775b04dca71e4da4d5c94cfdc55782946
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-04 19:49:29 +0300

    T6.3 äänestys toimii alustavasti

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ const App = () => {
 
   const vote = (id) => {
     console.log('vote', id)
+    dispatch(
+      {
+        type: 'VOTE',
+        payload: { id}
+      }
+    )
   }
 
   return (
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -23,7 +23,16 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch(action.type) {
+    case 'VOTE':
+      console.log('Vote',action.payload.id)
+      const id = action.payload.id
+      const anecdoteToVote = state.find(a => a.id === id)
+      const votedAnecdote = { ...anecdoteToVote, votes: anecdoteToVote.votes + 1}
+      return state.map(a => a.id !== id ? a : votedAnecdote)
+    default:
+      return state 
+  }
 }
 
 export default reducer
