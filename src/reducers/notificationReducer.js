260debcbd8e2452d7dad9b2c8af9c6a5529bad6d
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-06 15:04:11 +0300

    T6.13 ilmoitusta ei nyt näytetä turhaan

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = 'Default Notification'
+const initialState = null //'Default Notification'
 
 const notificationSlice = createSlice({
   name: 'notification',
@@ -8,9 +8,10 @@ const notificationSlice = createSlice({
   reducers: {
     setNotification(state, action) {
       return action.payload
-    }
+    },
+    hideNotification: (state) => null,
   },
 })
 
-export const { setNotification } = notificationSlice.actions
+export const { setNotification, hideNotification } = notificationSlice.actions
 export default notificationSlice.reducer
