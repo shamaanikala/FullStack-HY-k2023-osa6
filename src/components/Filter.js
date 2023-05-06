10d7240ed4a8614cce1fdfb0c97848294e116f13
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-06 09:40:54 +0300

    T6.9 suodinkentän tieto reduserille

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,13 @@
+import { filterChange } from '../reducers/filterReducer'
+import { useDispatch } from 'react-redux'
+
 const Filter = () => {
+  const dispatch = useDispatch()
+
   const handleChange = (event) => {
     // input-kentän arvo muuttujassa event.target.value
+    const query = event.target.value
+    dispatch(filterChange(query))
   }
   const style = {
     marginBottom: 10
