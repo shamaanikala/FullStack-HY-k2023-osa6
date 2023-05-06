a57d8aa3e5c97e14bff4d31b19c25e2af5be75c8
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-06 09:09:51 +0300

    T6.9 korjataan AnecdoteList komponentti

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -21,7 +21,7 @@ const AnecdoteList = () => {
   // ja kuten materiaalissa tehtiin importantNotes selektroissa
   // myös SO ketju tukee tätä:
   // https://stackoverflow.com/questions/34475367/where-should-i-handle-sorting-in-redux-app
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+  const anecdotes = useSelector(state => state.anecdotes.sort((a, b) => b.votes - a.votes))
   const dispatch = useDispatch()
 
   const vote = (id) => {
