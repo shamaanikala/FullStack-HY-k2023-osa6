948aed07f71fc146957171c50b77bd0297017ec8
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-07 18:31:09 +0300

    T6.20 haetaan React Querylla anekdootit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
 const App = () => {
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery(
+    'anecdotes',
+    () => axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+  )
+  console.log(result)
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
